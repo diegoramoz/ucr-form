8151dd6b9d414c3884bfc5c3fed8534bd887e714
Diego Ramos <54applets_hogs@icloud.com>
2026-04-17 10:22:52 -0400

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,55 @@
+import Form from "@/app/form";
+import { UcrTodoInput } from "@/app/validation";
+
+const tasks = [
+  {
+    taskId: "3",
+    name: "egg",
+    completed: "false",
+  },
+  {
+    taskId: "4",
+    name: "Ham",
+    completed: "false",
+  },
+];
+
+const defaultData: UcrTodoInput = {
+  todo: {
+    todoId: {
+      action: "ID",
+      value: "12",
+    },
+    name: {
+      action: "",
+      value: "Shopping List",
+    },
+    description: {
+      action: "",
+      value: "",
+    },
+  },
+
+  tasks: tasks.map((task) => ({
+    taskId: {
+      action: "ID",
+      value: task.taskId,
+    },
+    name: {
+      action: "",
+      value: task.name,
+    },
+    completed: {
+      action: "",
+      value: false,
+    },
+  })),
+};
+
+export default function Home() {
+  return (
+    <main className="max-w-screen-md mx-auto">
+      <Form defaultData={defaultData} />
+    </main>
+  );
+}
